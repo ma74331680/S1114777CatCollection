3ca3ad1d3318d30b60a00766bcbfac746e57c2ab
max792489 <s1114777@o365st.pu.edu.tw>
2024-10-07 23:59:44 +0800

    加入專案檔案。

---
+++ S1114777CatCollection/Controllers/CatsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using S1114777CatCollection.Models;
+
+namespace S1114777CatCollection.Controllers
+{
+    public class CatsController : Controller
+    {
+        private CatCollectionEntities db = new CatCollectionEntities();
+
+        // GET: Cats
+        public ActionResult Index()
+        {
+            return View(db.Cat.ToList());
+        }
+
+        // GET: Cats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // GET: Cats/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cats/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,fur,type,age")] Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cat.Add(cat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cat);
+        }
+
+        // GET: Cats/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // POST: Cats/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,fur,type,age")] Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        // GET: Cats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // POST: Cats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cat cat = db.Cat.Find(id);
+            db.Cat.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ S1114777CatCollection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S1114777CatCollection.MvcApplication" Language="C#" %>
